2fb464c1cbdd0dc4aab3ea8cf4a4615d0e133b35
AKP <ankitpandey2708@users.noreply.github.com>
2024-02-23 01:53:53 +0530

    pre/post fix

--- repo/js/javascript.js
+++ repo/js/javascript.js
@@ -8,3 +8,10 @@ alert( 2 * count++ );
 let abc = 1;
 abc++
 alert( 2 * abc );
+
+
+let apples = "2";
+let oranges = "3";
+
+// both values converted to numbers before the binary plus
+alert( +apples + +oranges ); 
