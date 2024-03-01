1699135129d96fc4f1ef600c29393617975a9fad
AKP <ankitpandey2708@users.noreply.github.com>
2024-03-02 00:13:33 +0530

    .

--- repo/js/javascript.js
+++ repo/js/javascript.js
@@ -1,7 +1,6 @@
 let counter = 1;
 alert( 2 * ++counter );
 
-
 let count = 1;
 alert( 2 * count++ );
 
@@ -9,9 +8,10 @@ let abc = 1;
 abc++
 alert( 2 * abc );
 
-
 let apples = "2";
 let oranges = "3";
-
-// both values converted to numbers before the binary plus
 alert( +apples + +oranges ); 
+
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+console.log(`can embed another ${str}` ); 
