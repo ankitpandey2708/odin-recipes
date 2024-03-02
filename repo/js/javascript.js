5c107af91805ebf46eb8adb97654a306b8314d9d
AKP <ankitpandey2708@users.noreply.github.com>
2024-03-03 02:33:45 +0530

    .

--- repo/js/javascript.js
+++ repo/js/javascript.js
@@ -12,6 +12,15 @@ let apples = "2";
 let oranges = "3";
 alert( +apples + +oranges ); 
 
+alert( "2" =='2' );  // true
+alert( "2" == 2 );  // true
+alert( null == undefined ); // true
+alert( null > 0 );  // (1) false
+alert( null == 0 ); // (2) false
+alert( null >= 0 ); // (3) true
+
+
+
 let str = "Hello";
 let str2 = 'Single quotes are ok too';
 console.log(`can embed another ${str}` ); 
