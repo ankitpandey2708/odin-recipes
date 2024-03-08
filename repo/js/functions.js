60a6c9a362ad8462fdc77562a2372f398845d849
AKP <ankitpandey2708@users.noreply.github.com>
2024-03-08 23:48:22 +0530

    functions in js

---
+++ repo/js/functions.js
@@ -0,0 +1,19 @@
+function add7(x) {
+return x+7
+}
+console.log( add7(1))
+
+function multiply(x,y) {
+    return x*y
+    }
+console.log( multiply(1,2))
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capitalize("gREETinGs")); 
+
+function lastLetter(x) {
+    return x.charAt(x.length-1)
+    }
+console.log(lastLetter('abcd'))
